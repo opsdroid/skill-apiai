509dc6038feb666f1b1fb3fb66f07fc23c364659
Jacob Tomlinson <jacob@jacobtomlinson.co.uk>
2016-12-15 17:13:20 +0000

    Removed duplicate declaration

--- apiai.py
+++ apiai.py
@@ -1,19 +1,17 @@
 from opsdroid.skills import match_apiai
-import logging
-import random
 
-@match_apiai('')
+@match_apiai('') # Matches all
 async def passthrough(opsdroid, message):
     if "action" in message.apiai["result"]:
+        action = message.apiai["result"]["action"]
+
         if "include" in opsdroid.config["skills"]["apiai"]:
             includes = opsdroid.config["skills"]["apiai"]["include"]
-            action = message.apiai["result"]["action"]
             if True not in [x in action for x in includes]:
                 return
 
         if "exclude" in opsdroid.config["skills"]["apiai"]:
             excludes = opsdroid.config["skills"]["apiai"]["exclude"]
-            action = message.apiai["result"]["action"]
             if True in [x in action for x in excludes]:
                 return
 
