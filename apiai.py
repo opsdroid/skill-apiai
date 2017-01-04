e432203cfb04d05178d0fd98a2147ce969248e4f
Jacob Tomlinson <jacob@jacobtomlinson.co.uk>
2017-01-04 16:53:56 +0000

    Catch empty include/exclude lists

--- apiai.py
+++ apiai.py
@@ -5,15 +5,21 @@ async def passthrough(opsdroid, message):
     if "action" in message.apiai["result"]:
         action = message.apiai["result"]["action"]
 
-        if "include" in opsdroid.config["skills"]["apiai"]:
-            includes = opsdroid.config["skills"]["apiai"]["include"]
-            if True not in [x in action for x in includes]:
-                return
+        try:
+            if "include" in opsdroid.config["skills"]["apiai"]:
+                includes = opsdroid.config["skills"]["apiai"]["include"]
+                if True not in [x in action for x in includes]:
+                    return
+        except TypeError:
+            pass
 
-        if "exclude" in opsdroid.config["skills"]["apiai"]:
-            excludes = opsdroid.config["skills"]["apiai"]["exclude"]
-            if True in [x in action for x in excludes]:
-                return
+        try:
+            if "exclude" in opsdroid.config["skills"]["apiai"]:
+                excludes = opsdroid.config["skills"]["apiai"]["exclude"]
+                if True in [x in action for x in excludes]:
+                    return
+        except TypeError:
+            pass
 
         if message.apiai["result"]["speech"]:
             await message.respond(message.apiai["result"]["speech"])
